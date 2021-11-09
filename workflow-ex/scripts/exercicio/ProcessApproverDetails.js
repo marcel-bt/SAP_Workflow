c4b2e38a4536a22d08577b7ea726b62ef8371ae0
“marcel-bt” <marcel.cvs@hotmail.com>
2021-11-09 20:02:37 +0000

    commit linha 8 Process approver details

--- workflow-ex/scripts/exercicio/ProcessApproverDetails.js
+++ workflow-ex/scripts/exercicio/ProcessApproverDetails.js
@@ -1,11 +1,11 @@
-$.context.internal.step.isRequired = false;
+$.context.internal.step.isRequired = true;
 var approverUserIDs = [];
 var approverGroupIDs = [];
 var ruleResult = $.context.internal.ruleresult.Result;
 
 if (ruleResult.length > 0 && ruleResult[0].ApproversList.length) {
     $.context.internal.step.approver = $.context.internal.ruleresult.Result[0].ApproversList[0];
-	$.context.internal.step.isRequired = $.context.internal.step.approver.ApprovalRequired;
+	//$.context.internal.step.isRequired = $.context.internal.step.approver.ApprovalRequired;
     for (var i = 0; i < ruleResult[0].ApproversList.length; i++) { 
         approverUserIDs.push(ruleResult[0].ApproversList[i].UserId);
         approverGroupIDs.push(ruleResult[0].ApproversList[i].GroupId);
